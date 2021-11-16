dec4bbe2232c2356d827349c5344c255c2295c84
Cian Gallagher <cian.gallagher@zendesk.com>
2021-11-16 22:38:01 +0000

    Add repl and main.go file.

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"os/user"
 
+	"github.com/cian911/platoon/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! Thank you for using the Platton programming language.\n", user.Username)
+
+	// Start the REPL
+	repl.Start(os.Stdin, os.Stdout)
 }
---
+++ repl/repl.go
@@ -0,0 +1,38 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/cian911/platoon/lexer"
+	"github.com/cian911/platoon/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprint(out, PROMPT)
+		scanned := scanner.Scan()
+
+		if !scanned {
+			return
+		}
+
+		// Get the new latest line and pass it to our lexer
+		line := scanner.Text()
+
+		if line == "exit" {
+			return
+		}
+
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
+}
