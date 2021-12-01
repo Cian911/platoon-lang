01c49d5923413cfc6e9ee1faf2e8653b9bfa3331
Cian Gallagher <cian.gallagher@zendesk.com>
2021-12-01 23:14:48 +0000

    Enable AST to support String() method on main Node interface.

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,14 @@
 package ast
 
-import "github.com/cian911/platoon/token"
+import (
+	"bytes"
+
+	"github.com/cian911/platoon/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -20,6 +25,17 @@ type Program struct {
 	Statements []Statement
 }
 
+// Statisfy the Node interface by implementing the String() method
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 // This program node is going to be the root node of every tree our
 // ast will parse.
 func (p *Program) TokenLiteral() string {
@@ -28,6 +44,23 @@ func (p *Program) TokenLiteral() string {
 	} else {
 		return ""
 	}
+}
+
+// ExpressionStatement
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (ls *ExpressionStatement) statementNode()       {}
+func (ls *ExpressionStatement) TokenLiteral() string { return ls.Token.Literal }
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+
+	return ""
 }
 
 // Let Statement
@@ -40,6 +73,22 @@ type LetStatement struct {
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
 type Identifier struct {
 	Token token.Token // token.IDENT
 	Value string
@@ -47,6 +96,8 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+func (i *Identifier) String() string { return i.Value }
 
 // Return Statement
 type ReturnStatement struct {
@@ -56,3 +107,17 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.Value != nil {
+		out.WriteString(rs.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
--- ast/ast_test.go
+++ ast/ast_test.go
@@ -1,1 +1,31 @@
 package ast
+
+import (
+	"testing"
+
+	"github.com/cian911/platoon/token"
+)
+
+func TestString(t *testing.T) {
+	input := "let myVar = anotherVar;"
+
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+
+	if program.String() != input {
+		t.Errorf("program.String() did not have the correct value. Want %v, Got %v", input, program.String())
+	}
+}
